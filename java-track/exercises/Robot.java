a30b9fc840d50d97d8dc69e32f49c98e09edaebc
Bram <bramboettge@gmail.com>
2016-10-06 20:05:44 -0500

    initial commit

---
+++ java-track/exercises/Robot.java
@@ -0,0 +1,36 @@
+
+public class Robot {
+	
+	//Fields:
+	
+	private String name;
+	private int position;
+	private int speed;
+	private int orientation;
+	
+	//Behaviors
+	
+	//Create a robot
+	public Robot(String name, int position, int speed, int orientation)
+		this.name = name;
+		this.position = position;
+		this.speed = speed;
+		this.orientation = orientation;
+		
+	//Get name
+	//Get Position
+	//Get Speed
+	//Get Orientation
+	//Move
+	//Rotate
+	//Determine distance from other robot
+	//Display as String
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
